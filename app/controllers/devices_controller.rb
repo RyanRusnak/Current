1edb813e0998d2883b9f23c7d3230a330960efb7
Ryan Rusnak <ryandrusnak@gmail.com>
2012-06-11 14:52:00 -0400

    basic structure

---
+++ app/controllers/devices_controller.rb
@@ -0,0 +1,86 @@
+class DevicesController < ApplicationController
+  # GET /devices
+  # GET /devices.xml
+  def index
+    @devices = Device.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @devices }
+    end
+  end
+
+  # GET /devices/1
+  # GET /devices/1.xml
+  def show
+    @device = Device.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @device }
+    end
+  end
+
+  # GET /devices/new
+  # GET /devices/new.xml
+  def new
+    @device = Device.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @device }
+    end
+  end
+
+  # GET /devices/1/edit
+  def edit
+    @customer = Customer.find(params[:customer_id])
+    @device = @customer.devices.find(params[:id])
+  end
+
+  # POST /devices
+  # POST /devices.xml
+  def create
+    @customer = Customer.find(params[:customer_id])
+    @device = @customer.devices.new(params[:device])
+
+    respond_to do |format|
+      if @device.save
+        format.html { redirect_to(@customer, :notice => 'Device was successfully created.') }
+        format.xml  { render :xml => @device, :status => :created, :location => @device }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @device.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /devices/1
+  # PUT /devices/1.xml
+  def update
+    @customer = Customer.find(params[:customer_id])
+    @device = @customer.devices.find(params[:id])
+
+    respond_to do |format|
+      if @device.update_attributes(params[:device])
+        format.html { redirect_to([@customer,@device], :notice => 'Device was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @device.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /devices/1
+  # DELETE /devices/1.xml
+  def destroy
+    @device = Device.find(params[:id])
+    @device.destroy
+
+    respond_to do |format|
+      format.html  { redirect_to(customer_path(@device.customer_id)) }
+      format.xml  { head :ok }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CurrentDeviceList::Application.config.secret_token = '35fa62b900dcc397db85c4326b5cccbdd2211337836df6adf1dbcc20ca483bf86b22fbffed5622422da7fb0e80ee7405d6d60cd64d3243e7b712c6bddc74a7c0'
